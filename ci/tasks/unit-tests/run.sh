10213792469370199ba71af9891f4f2e5d1135d0
Keaty Gross <kgross@pivotal.io>
2016-01-22 11:36:43 -0500

    fixing paths
    [#111826579]
    
    Signed-off-by: Emily Casey <ecasey@pivotal.io>

--- ci/tasks/unit-tests/run.sh
+++ ci/tasks/unit-tests/run.sh
@@ -2,12 +2,11 @@
 
 set -e
 export GO15VENDOREXPERIMENT=1
-working_dir=$(cd `dirname $0` && cd ../ && pwd)
-export GOPATH=$working_dir/go
+export GOPATH=$PWD/go
 
-mkdir -p $working_dir/go/src/github/pivotal-cf
-cp -r cf-watch go/src/github/pivotal-cf/cf-watch
-pushd go/src/github/pivotal-cf/cf-watch > /dev/null
+mkdir -p go/src/github.com/pivotal-cf
+cp -r cf-watch go/src/github.com/pivotal-cf/
+pushd go/src/github.com/pivotal-cf/cf-watch > /dev/null
   go install ./vendor/github.com/onsi/ginkgo/ginkgo
   go install ./vendor/github.com/onsi/gomega
   ginkgo -r ./*
