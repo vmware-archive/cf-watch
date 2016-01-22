becc183f4c88b3e57049fa3ef712484192ee8bc1
Emily Casey <ecasey@pivotal.io>
2016-01-22 11:28:48 -0500

    adding -r flag to cp
    [#111826579]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- ci/tasks/unit-tests/run.sh
+++ ci/tasks/unit-tests/run.sh
@@ -6,7 +6,7 @@ working_dir=$(cd `dirname $0` && cd ../ && pwd)
 export GOPATH=$working_dir/go
 
 mkdir -p $working_dir/go/src/github/pivotal-cf
-cp cf-watch go/src/github/pivotal-cf/cf-watch
+cp -r cf-watch go/src/github/pivotal-cf/cf-watch
 pushd go/src/github/pivotal-cf/cf-watch > /dev/null
   go install ./vendor/github.com/onsi/ginkgo/ginkgo
   go install ./vendor/github.com/onsi/gomega
