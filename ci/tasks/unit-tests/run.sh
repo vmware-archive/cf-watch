d345c55807c25d998d0a4e658d9de868f601386b
Emily Casey <ecasey@pivotal.io>
2016-01-22 12:08:33 -0500

    adding gopath to path
    [#111826579]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- ci/tasks/unit-tests/run.sh
+++ ci/tasks/unit-tests/run.sh
@@ -3,15 +3,13 @@
 set -ex
 export GO15VENDOREXPERIMENT=1
 export GOPATH=$PWD/go
+export PATH=$GOPATH:$PATH
 
 mkdir -p go/src/github.com/pivotal-cf
 cp -r cf-watch go/src/github.com/pivotal-cf/
 
-ls -R 
 pushd go/src/github.com/pivotal-cf/cf-watch > /dev/null
-  echo $GOPATH
   go install ./vendor/github.com/onsi/ginkgo/ginkgo
   go install ./vendor/github.com/onsi/gomega
-  ls -R $GOPATH
   ginkgo -r ./*
 popd > /dev/null
