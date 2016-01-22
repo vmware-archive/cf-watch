9b281c8987d836473501e005b64a45fc3566a8d6
Emily Casey <ecasey@pivotal.io>
2016-01-22 11:16:16 -0500

    adding pipeline for cf-watch with unit-test task
    [#111826579]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

---
+++ ci/tasks/unit-tests/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+export GO15VENDOREXPERIMENT=1
+working_dir=$(cd `dirname $0` && cd ../ && pwd)
+export GOPATH=$working_dir/go
+
+mkdir -p $working_dir/go/src/github/pivotal-cf
+cp cf-watch go/src/github/pivotal-cf/cf-watch
+pushd go/src/github/pivotal-cf/cf-watch > /dev/null
+  go install ./vendor/github.com/onsi/ginkgo/ginkgo
+  go install ./vendor/github.com/onsi/gomega
+  ginkgo -r ./*
+popd > /dev/null
