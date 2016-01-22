b2f0e8313b36271638cfd2c0a0bf01192c1b9dcd
Emily Casey <ecasey@pivotal.io>
2016-01-22 11:55:09 -0500

    debugging...
    [#111826579]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- ci/tasks/unit-tests/run.sh
+++ ci/tasks/unit-tests/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 export GO15VENDOREXPERIMENT=1
 export GOPATH=$PWD/go
 
 mkdir -p go/src/github.com/pivotal-cf
 cp -r cf-watch go/src/github.com/pivotal-cf/
+
+ls **/*
 pushd go/src/github.com/pivotal-cf/cf-watch > /dev/null
+  echo $GOPATH
   go install ./vendor/github.com/onsi/ginkgo/ginkgo
   go install ./vendor/github.com/onsi/gomega
   ginkgo -r ./*
